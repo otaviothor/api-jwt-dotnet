bac135a2355c1eb79bcafa473401c9504b3eb16a
Otávio Silva <otaviosilva2632@gmail.com>
2021-12-11 12:09:15 -0300

    feat: create user repository

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ApiAuth.Models;
+
+namespace ApiAuth.Repositories
+{
+  public static class UserRepository
+  {
+    public static User Get(string username, string password)
+    {
+      var users = new List<User>
+      {
+        new() {Id = 1, Username = "batman", Password = "batman", Role = "manager"},
+        new() {Id = 2, Username = "robin", Password = "robin", Role = "employee"}
+      };
+
+      return users
+        .FirstOrDefault(x =>
+          x.Username == username && x.Password == password);
+    }
+  }
+}
